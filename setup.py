1213265097d43d60927d0ece1ada02f4637a2cd8
Dariusz Choruzy <dariusz.choruzy@gmail.com>
2021-09-12 17:03:31 +0200

    installable pypi package

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hltv-data",
+    version="0.0.1",
+    author="Dariusz Choruzy",
+    author_email="dariusz.choruzy@gmail.com",
+    description="HLTV.org data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dchoruzy/hltv-data",
+    project_urls={
+        "Bug Tracker": "https://github.com/dchoruzy/hltv-data/issues",
+    },
+    license='MIT License',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
